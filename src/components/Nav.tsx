bdaa924420b1cd789f1449b02f08a2bba0cd1816
dont3t <jorcion@gmail.com>
2020-12-28 14:11:18 +0800

    使用 svg-sprite-loader 加载 svg icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/chart.svg');
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -11,8 +15,15 @@ const NavWrapper = styled.nav`
 
     > li {
       flex: 1;
-      text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 4px 0;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,12 +32,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/money">Money</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#label'/>
+          </svg>
           <Link to="/tags">Tags</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#chart'/>
+          </svg>
           <Link to="/statistics">Statistics</Link>
         </li>
       </ul>
